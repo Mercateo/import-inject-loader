d60bb5101fc5d3e576e636bd8f84b6ff84a903f9
Philipp Zins <philipp.zins@mercateo.com>
2018-01-05 10:37:47 +0100

    try to fix #4

--- examples/src/get-hello-world.js
+++ examples/src/get-hello-world.js
@@ -1,3 +1,5 @@
 import { HELLO_WORLD } from 'rfnd-hello-world';
+import * as HELLO_WORLD_DEFAULT from './hello-world';
 
 export const getHelloWorld = () => HELLO_WORLD;
+export const getHelloWorldDefault = () => HELLO_WORLD_DEFAULT;
--- examples/tests/get-hello-world.js
+++ examples/tests/get-hello-world.js
@@ -1,18 +1,24 @@
 import expect from 'expect';
 import {
   getHelloWorld,
+  getHelloWorldDefault,
   ilOverwriteHELLO_WORLD,
+  ilOverwriteHELLO_WORLD_DEFAULT,
   ilResetAll
-} from 'import-inject-loader?HELLO_WORLD!../src/get-hello-world';
+} from 'import-inject-loader?HELLO_WORLD,HELLO_WORLD_DEFAULT!../src/get-hello-world';
 
 describe('getHelloWorld()', () => {
   it('should return "Hello world!"', () => {
     expect(getHelloWorld()).toBe('Hello world!');
+    expect(getHelloWorldDefault()).toBe('Hello world!');
   });
 
   it('should return "Mocked world!"', () => {
     ilOverwriteHELLO_WORLD('Mocked world!');
     expect(getHelloWorld()).toBe('Mocked world!');
+
+    ilOverwriteHELLO_WORLD_DEFAULT('Mocked world!');
+    expect(getHelloWorldDefault()).toBe('Mocked world!');
   });
 
   afterEach(ilResetAll);
